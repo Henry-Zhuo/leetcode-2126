Henry-Zhuo/leetcode-2126

commit d8afc22ad0674cbf268db6c72d01edd63bf3b9a7

Use long long int for mass
- Leetcode allows long long int mass to be in the function signature for submission, so there's no need to cast to long long int

main.c
@@ -14,12 +14,7 @@ enum RETURN_CODES {
 
 
 
-static bool asteroidsDestroyedLongLong(long long int mass, int* asteroids, int asteroidsSize);
-static bool asteroidsDestroyed(int mass, int* asteroids, int asteroidsSize);
-
-
-
-/* Helper function for problem Leetcode 2126.
+/* Solution for problem Leetcode 2126.
     This is similar to quicksort, but we absorb asteroids in the left subarray
     and recurse only on the second subarray. The absorbing happens immediately,
     so there is no left subarray to keep track of.
@@ -27,7 +22,8 @@ static bool asteroidsDestroyed(int mass, int* asteroids, int asteroidsSize);
     TODO: Benchmark if it is faster to absorb asteroids after the first loop
     ends, using a dedicated for loop since that can be vectorized.
 */
-bool asteroidsDestroyedLongLong(long long int mass, int* asteroids, int asteroidsSize) {
+bool asteroidsDestroyed(long long int mass, int* asteroids, int asteroidsSize) {
+    bool swapped = false;
     int rightSubarrayStartIdx;
     int scanRightSideIdx = asteroidsSize - 1;
 
@@ -36,8 +32,6 @@ bool asteroidsDestroyedLongLong(long long int mass, int* asteroids, int asteroid
             // Can absorb asteroid
             mass += asteroids[rightSubarrayStartIdx];
         } else {
-            bool swapped = false;
-
             for ( ; scanRightSideIdx > rightSubarrayStartIdx; scanRightSideIdx--) {
                 if (mass >= asteroids[scanRightSideIdx]) {
                     mass += asteroids[scanRightSideIdx];
@@ -71,17 +65,6 @@ bool asteroidsDestroyedLongLong(long long int mass, int* asteroids, int asteroid
         asteroids + rightSubarrayStartIdx,
         asteroidsSize - rightSubarrayStartIdx
     );
-}
-
-
-
-/* Submission for problem Leetcode 2126
-    Using int input types because that's what Leetcode provides.
-*/
-bool asteroidsDestroyed(int mass, int* asteroids, int asteroidsSize) {
-    // Leetcode gives an error for integer overflow, use long long ints
-    long long int currentMass = mass;
-    return asteroidsDestroyedLongLong(currentMass, asteroids, asteroidsSize);
 }
 
 

commit 04d8ea7ab568b3cba0e55e16f517aa97cd94a1f8

Reset swapped to false when scanning right side
- fixes: d8afc22ad0674cbf268db6c72d01edd63bf3b9a7

main.c
@@ -29,7 +29,7 @@ enum RETURN_CODES {
     ends, using a dedicated for loop since that can be vectorized.
 */
 bool asteroidsDestroyed(long long int mass, int* asteroids, int asteroidsSize) {
-    bool swapped = false;
+    bool swapped;
     int rightSubarrayStartIdx;
     int scanRightSideIdx = asteroidsSize - 1;
 
@@ -38,6 +38,8 @@ bool asteroidsDestroyed(long long int mass, int* asteroids, int asteroidsSize) {
             // Can absorb asteroid
             mass += asteroids[rightSubarrayStartIdx];
         } else {
+            swapped = false;
+
             for ( ; scanRightSideIdx > rightSubarrayStartIdx; scanRightSideIdx--) {
                 if (mass >= asteroids[scanRightSideIdx]) {
                     mass += asteroids[scanRightSideIdx];